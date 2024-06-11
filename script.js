6b4d5d6a888b0adfb58582dda8a99c76ad289c63
Wilson1519 <wilsontv1519@gmail.com>
2024-06-11 19:46:16 -0400

    juego

--- script.js
+++ script.js
@@ -1,91 +1,117 @@
-let banderas = ["c.svg", "c++.svg", "csharp.svg", "java.svg", "javascript.svg","css.svg","html.svg","python.svg","ruby.svg"];
-let opciones = [];
-//opciones a mostrar en cada jugada
-opciones.push(["JAVASCRIPT", "C", "C++"]);
-opciones.push(["C", "C--", "C++"]);
-opciones.push(["CSHAR", "C#", "C++"]);
-opciones.push(["JAVA", "JAVASCRIPT", "PHP"]);
-opciones.push(["JAVASCRIPT", "PHP", "JAVA"]);
-opciones.push(["HTML", "CSS", "C++"]);
-opciones.push(["PYTHON", "PHP", "HTML"]);
-opciones.push(["RUBY", "PYTHON", "CSS"]);
-opciones.push(["NETBEANS", "ECLIPSE", "RUBY"]);
+//Sellecionar los elemnetos del DOM//
+let num1 = document.querySelector("#num1");
+let num2 = document.querySelector("#num2");
+let respuesta_usuario = document.querySelector("#respuesta_usuario");
+let msj_correcion = document.querySelector("#msj_correcion");
+let operacion = document.querySelector("#operacion");
+let operacion_actual;
+let n1, n2; //Guarda numeros generados aleatoriamente
 
-let correcta = [1,2,1,0,0,1,2,1,2]; 
-//variable que guarda la posicion actual 
-let posActual = 0;
-//variable que guarda la cantidad acertadas hasta el momento 
-let cantidadAcertadas = 0;
-
-function comenzarJuego(){
-//reseteamos las variables 
-    posActual = 0;
-    cantidadAcertadas = 0;
-    
-//activamos las pantallas necesarias 
-    document.getElementById("pantalla-inicial").style.display = "none"; 
-    document.getElementById("pantalla-juego").style.display = "block"; cargarBandera();
+//Funcion para la suma
+function btnSumar(){
+    msj_correcion.innerHTML = "";//limpiando el div
+    activarBoton("suma");//agregamos la clase activa al boton suma y quita el resta
+    operacion_actual = "+";
+    operacion.innerHTML = " + ";
+    nuevaSuma();//generamos los numeros aleatorios
 }
+function nuevaSuma(){
+    n1 = parseInt(Math.random()*10);//genera numeros de 0 a 9
+    n2 = parseInt(Math.random()*10);//genera numeros de 0 a 9
+    num1.innerHTML = n1;//asignamos los numeros generados
+    num2.innerHTML = n2;
+    respuesta_usuario.focus();//el curso apunta al input
+}
+function btnProducto(){
+    msj_correcion.innerHTML = "";
+    activarBoton("producto");
+    operacion_actual = "*";
+    operacion.innerHTML = " x ";
+    nuevoProducto();
+}
+function nuevoProducto(){
+    n1 = parseInt(Math.random()*10);
+    n2 = parseInt(Math.random()*10);
+    num1.innerHTML = n1;
+    num2.innerHTML = n2;
+    respuesta_usuario.focus();
+}
+function btnResta(){
+    msj_correcion.innerHTML = "";
+    activarBoton("resta");
+    operacion_actual = "-";
+    operacion.innerHTML = " - ";
+    nuevaResta();
+}
+function nuevaResta(){
+    n1 = parseInt(Math.random()* 5 + 5);
+    n2 = parseInt(Math.random()* 5);
+    num1.innerHTML = n1;
+    num2.innerHTML = n2;
+    respuesta_usuario.focus();
+}
+function btnDivision(){
+    msj_correcion.innerHTML = "";
+    activarBoton("division");
+    operacion_actual = "/";
+    operacion.innerHTML = " / ";
+    nuevaDivision();
+}
+function nuevaDivision(){
+    let divisores = [];
+    n1 = parseInt(Math.random()* 9 + 1);
+    for(var i=1; i<=n1; i++){
+        if (n1 % i === 0){
+            divisores.push(i);
+        }
+    }
+    let pos = parseInt(Math.random() * (divisores.length));
+    n2 = divisores[pos];
 
-//funcion que carga la siguiente bandera y sus opciones 
-function cargarBandera(){
+    num1.innerHTML = n1;
+    num2.innerHTML = n2;
+    respuesta_usuario.focus();
+}
+function corregir() {
+    //cuando el usuario no ha ingresado nada
+    if(respuesta_usuario.value == ""){
+        return;
+    }
 
-    //controlo sis se acabaron las banderas 
-    if(banderas.length <= posActual){
-        terminarJuego();
-    }else{//cargo las opciones
-        
-        //limpiamos las clases que se asignaron 
-        limpiarOpciones();
-        document.getElementById("imgBandera").src = "img/" + banderas[posActual]; 
-        document.getElementById("n0").innerHTML = opciones[posActual][0]; 
-        document.getElementById("n1").innerHTML = opciones[posActual][1]; 
-        document.getElementById("n2").innerHTML = opciones[posActual][2];
+    let solucion;
+    let operacion = n1 + operacion_actual + n2;
+    solucion = eval(operacion);
+
+    var i = document.createElement("i");
+    if(respuesta_usuario.value == solucion){
+        i.className = "fa-regular fa-face-grin"
+    }else{
+        i.className = "fa-regular fa-face-frown"
+    }
+    msj_correcion.appendChild(i);
+
+    if(operacion_actual == "+"){
+        nuevaSuma();
+    }else if(operacion_actual == "-"){
+        nuevaResta();
+    }else if(operacion_actual == "*"){
+        nuevoProducto();
+    }else if(operacion_actual == "/"){
+        nuevaDivision();
+    }
+    respuesta_usuario.value = "";
+}
+respuesta_usuario.onkeydown = function(e){
+    var ev = document.all ? window.event:e;
+    if(ev.keyCode == 13){
+        corregir();
     }
 }
-
-function limpiarOpciones(){
-     document.getElementById("n0").className = "nombre"; 
-     document.getElementById("n1").className = "nombre"; 
-     document.getElementById("n2").className = "nombre";
-
-    document.getElementById("l0").className = "letra"; 
-    document.getElementById("l1").className = "letra"; 
-    document.getElementById("l2").className = "letra";
+function activarBoton(idBoton) {
+    document.getElementById("suma").className = "";
+    document.getElementById("resta").className = "";
+    document.getElementById("producto").className = "";
+    document.getElementById("division").className = "";
+    document.getElementById(idBoton).className = "activado";
 }
 
-function comprobarRespuesta(opElegida){ 
-    if(opElegida==correcta[posActual]){//acertó
-    //agregamos las clases para colocar el color verde a la opcion elegida 
-    document.getElementById("n" + opElegida).className = "nombre nombreAcertada"; 
-    document.getElementById("l" + opElegida).className = "letra letraAcertada"; 
-    cantidadAcertadas++;
-    }else{//no acerto
-    //agregamos las clases para colocar en rojo la opcion elegida 
-    document.getElementById("n" + opElegida).className = "nombre nombreNoAcertada";
-    document.getElementById("l" + opElegida).className = "letra letraNoAcertada";
-    
-    //opcion que era correcta
-    document.getElementById("n" + correcta[posActual]).className = "nombrenombreAcertada";
-    document.getElementById("l" + correcta[posActual]).className = "letraletraAcertada";
-    }
-    posActual++;
-    //Esperamos 1 segundo y pasamos mostrar la siguiente bandera y sus opciones 
-    setTimeout(cargarBandera, 1000);
-}
-function terminarJuego(){
-    //ocultamos las pantallas y mostramos la pantalla final 
-    document.getElementById("pantalla-juego").style.display = "none"; 
-    document.getElementById("pantalla-final").style.display = "block";
-    //agreamos los resultados 
-    document.getElementById("numCorrectas").innerHTML = cantidadAcertadas; 
-    document.getElementById("numIncorrectas").innerHTML = banderas.length - cantidadAcertadas;
-}
-function volverAlInicio(){
-    //ocultamos las pantallas y activamos la inicial 
-    document.getElementById("pantalla-final").style.display = "none"; 
-    document.getElementById("pantalla-inicial").style.display = "block"; 
-    document.getElementById("pantalla-juego").style.display = "none";
-}
-    
-    
